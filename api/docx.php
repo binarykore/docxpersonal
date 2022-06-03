7a57773b5ae8b01cdeacdd2b0f3e09c289131374
Jaedee Delfin <binarykorra@icloud.com>
2022-06-03 14:53:19 +0800

    0

--- api/docx.php
+++ api/docx.php
@@ -13,15 +13,27 @@ if(!empty($_GET["filename"]) && !empty($_GET["year"])){
 }elseif(empty($_GET["filename"]) || empty($_GET["year"])){
 	$_dir = scandir($_SERVER["DOCUMENT_ROOT"]."/api/docx/");
 	$_year = [];
+	$_yearDir = [];
+	$_blog = [];
 	echo("<title>Blog Docx Tab</title>");
 	echo("<code>Coming Soon...</code>");
 	foreach($_dir as $_token => $_coin){
 		if($_coin == "." || $_coin == ".."){
 			continue;
 		}else{
+			$_yearDir[] = (scandir($_SERVER["DOCUMENT_ROOT"]."/api/docx/".$_coin."/"));
 			$_year[] = ($_coin);
 		}
 	}
-	print_r($_year);
+	for($_g = 0;$_g <= count($_year)-1;$_g++){
+		foreach($_yearDir[$_g] as $_token => $_coin){
+			if($_coin == "." || $_coin == ".."){
+				continue;
+			}else{
+				$_blog[] = ("//".$_SERVER["HTTP_HOST"]."/blog/".($_coin)."/".($_g));
+			}
+		}
+	}	
+	print_r($_blog);
 }//
 ?>
